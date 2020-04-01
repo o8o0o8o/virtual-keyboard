55587c149bb6ffabd05258fff5b943a35116e374
Siarhei Labuts <sergeylabut@gmail.com>
2020-04-01 20:39:41 +0300

    fix:eslint

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-function init() {
-  localStorage.clear();
-  renderKeyboard(false, false);
-}
 
 function renderKeyboard(toggleLang, shift) {
-  let text =
-    document.getElementById('text') !== null
-      ? document.getElementById('text').value
-      : '';
+  const text = document.getElementById('text') !== null
+    ? document.getElementById('text').value
+    : '';
   const main = document.getElementById('main');
   main.innerHTML = '';
-  let rows = [];
+  const rows = [];
   const textField = document.createElement('textarea');
   textField.id = 'text';
   textField.value = text;
@@ -34,10 +29,10 @@ function renderKeyboard(toggleLang, shift) {
   } else {
     cent = 'central';
   }
-  for (let item in keys) {
-    let el = document.createElement('div');
+  for (const item in keys) {
+    const el = document.createElement('div');
     el.setAttribute('onclick', 'handleClick(id)');
-    let central = document.createElement('span');
+    const central = document.createElement('span');
     el.id = item;
     central.classList.add('central');
     central.innerHTML = keys[item][lang][cent];
@@ -60,61 +55,58 @@ function renderKeyboard(toggleLang, shift) {
 
 function arrowHelper() {
   text.focus();
-        let pos = text.selectionEnd;
-        let start = 0;
-        let end = 0;
-        let counter = pos;
-        if (text.value.charAt(pos) === '\n') {
-       counter = pos - 1;
-          end = pos;
-        } else {
-          end =
-          text.value.indexOf('\n', pos) !== -1
-            ? text.value.indexOf('\n', pos) - 1
-            : text.value.length;
-        }
-        for (let i = counter; i >= 0; i--) {
-          if (text.value.charAt(i) === '\n') {
-            start = i + 1;
-            break;
-          } else {
-            start = i;
-          }
-        }
-        let line = 0;
-        for (let i = 0; i < pos; i++) {
-          if (text.value.charAt(i) === '\n') {
-            line++;
-          }
-        }
-        let index = pos - start;
-        let temp = text.value.split('\n');
-        let arr = [];
-        for (let i = 0; i < temp.length; i++) {
-          arr.push(temp[i].split(''));
-        }
-       return {
-         start,
-         pos,
-         end,
-         index,
-         arr,
-         line,
-       }
+  const pos = text.selectionEnd;
+  let start = 0;
+  let end = 0;
+  let counter = pos;
+  if (text.value.charAt(pos) === '\n') {
+    counter = pos - 1;
+    end = pos;
+  } else {
+    end = text.value.indexOf('\n', pos) !== -1
+      ? text.value.indexOf('\n', pos) - 1
+      : text.value.length;
+  }
+  for (let i = counter; i >= 0; i--) {
+    if (text.value.charAt(i) === '\n') {
+      start = i + 1;
+      break;
+    } else {
+      start = i;
+    }
+  }
+  let line = 0;
+  for (let i = 0; i < pos; i++) {
+    if (text.value.charAt(i) === '\n') {
+      line++;
+    }
+  }
+  const index = pos - start;
+  const temp = text.value.split('\n');
+  const arr = [];
+  for (let i = 0; i < temp.length; i++) {
+    arr.push(temp[i].split(''));
+  }
+  return {
+    start,
+    pos,
+    end,
+    index,
+    arr,
+    line,
+  };
 }
 
 function handleClick(id) {
-  let button = document.getElementById(id);
-  let text = document.getElementById('text');
+  const button = document.getElementById(id);
+  const text = document.getElementById('text');
   switch (id) {
     case 'Backspace':
       text.selectionStart !== text.selectionEnd
-        ? (text.value =
-            text.value.substring(0, text.selectionStart) +
-            text.value.substring(text.selectionEnd + 1))
-        : (text.value =
-            text.value.substring(0, text.selectionEnd - 1) +
-            text.value.substring(text.selectionEnd));
+        ? (text.value = text.value.substring(0, text.selectionStart)
+            + text.value.substring(text.selectionEnd + 1))
+        : (text.value = text.value.substring(0, text.selectionEnd - 1)
+            + text.value.substring(text.selectionEnd));
       break;
     case 'Tab':
       text.value += '        ';
@@ -123,18 +115,17 @@ function handleClick(id) {
       text.value += '\n';
       break;
     case 'Delete':
-      text.value =
-        text.value.substring(0, text.selectionStart) +
-        text.value.substring(text.selectionEnd + 1);
+      text.value = text.value.substring(0, text.selectionStart)
+        + text.value.substring(text.selectionEnd + 1);
       break;
     case 'ArrowUp':
       {
-        let start = arrowHelper().start;
-        let pos = arrowHelper().pos;
-        let end = arrowHelper().end;
-        let line = arrowHelper().line;
-        let arr = arrowHelper().arr;
-        let index = arrowHelper().index;
+        const { start } = arrowHelper();
+        const { pos } = arrowHelper();
+        const { end } = arrowHelper();
+        const { line } = arrowHelper();
+        const { arr } = arrowHelper();
+        const { index } = arrowHelper();
         let calcPos = 0;
         for (let i = 0; i < line - 1; i++) {
           calcPos += arr[i].length + 1;
@@ -151,18 +142,18 @@ function handleClick(id) {
     case 'ArrowLeft':
       {
         text.focus();
-        text.selectionStart = text.selectionStart - 1;
-        text.selectionEnd = text.selectionEnd - 1;
+        text.selectionStart -= 1;
+        text.selectionEnd -= 1;
       }
       break;
     case 'ArrowDown':
       {
-        let start = arrowHelper().start;
-        let pos = arrowHelper().pos;
-        let end = arrowHelper().end;
-        let line = arrowHelper().line;
-        let arr = arrowHelper().arr;
-        let index = arrowHelper().index;
+        const { start } = arrowHelper();
+        const { pos } = arrowHelper();
+        const { end } = arrowHelper();
+        const { line } = arrowHelper();
+        const { arr } = arrowHelper();
+        const { index } = arrowHelper();
         let calcPos = 0;
         for (let i = 0; i < line + 1; i++) {
           calcPos += arr[i].length + 1;
@@ -179,8 +170,8 @@ function handleClick(id) {
     case 'ArrowRight':
       {
         text.focus();
-        text.selectionStart = text.selectionStart + 1;
-        text.selectionEnd = text.selectionEnd + 1;
+        text.selectionStart += 1;
+        text.selectionEnd += 1;
       }
       break;
     case 'CapsLock':
@@ -199,7 +190,7 @@ function handleClick(id) {
   text.setAttribute('autofocus', 'autofocus');
   button.classList.toggle('highlight');
   setTimeout(() => {
-    let button = document.getElementById(id);
+    const button = document.getElementById(id);
     button.classList.toggle('highlight');
   }, 100);
 }
@@ -209,323 +200,323 @@ function keysGen() {
     Backquote: {
       eng: { central: '`', secondary: '~' },
       rus: { central: 'ё', secondary: 'Ё' },
-      weight: 1
+      weight: 1,
     },
     Digit1: {
       eng: { central: '1', secondary: '!' },
       rus: { central: '1', secondary: '!' },
-      weight: 1
+      weight: 1,
     },
     Digit2: {
       eng: { central: '2', secondary: '@' },
       rus: { central: '2', secondary: "'" },
-      weight: 1
+      weight: 1,
     },
     Digit3: {
       eng: { central: '3', secondary: '#' },
       rus: { central: '3', secondary: '№' },
-      weight: 1
+      weight: 1,
     },
     Digit4: {
       eng: { central: '4', secondary: '$' },
       rus: { central: '4', secondary: ';' },
-      weight: 1
+      weight: 1,
     },
     Digit5: {
       eng: { central: '5', secondary: '%' },
       rus: { central: '5', secondary: '%' },
-      weight: 1
+      weight: 1,
     },
     Digit6: {
       eng: { central: '6', secondary: ':' },
       rus: { central: '6', secondary: '^' },
-      weight: 1
+      weight: 1,
     },
     Digit7: {
       eng: { central: '7', secondary: '?' },
       rus: { central: '7', secondary: '&' },
-      weight: 1
+      weight: 1,
     },
     Digit8: {
       eng: { central: '8', secondary: '*' },
       rus: { central: '8', secondary: '*' },
-      weight: 1
+      weight: 1,
     },
     Digit9: {
       eng: { central: '9', secondary: '(' },
       rus: { central: '9', secondary: '(' },
-      weight: 1
+      weight: 1,
     },
     Digit0: {
       eng: { central: '0', secondary: ')' },
       rus: { central: '0', secondary: ')' },
-      weight: 1
+      weight: 1,
     },
     Minus: {
       eng: { central: '-', secondary: '_' },
       rus: { central: '-', secondary: '_' },
-      weight: 1
+      weight: 1,
     },
     Equal: {
       eng: { central: '=', secondary: '+' },
       rus: { central: '=', secondary: '+' },
-      weight: 1
+      weight: 1,
     },
     Backspace: {
       eng: { central: 'backspace', secondary: 'backspace' },
       rus: { central: 'backspace', secondary: 'backspace' },
-      weight: 2.5
+      weight: 2.5,
     },
     Tab: {
       eng: { central: 'tab', secondary: 'tab' },
       rus: { central: 'tab', secondary: 'tab' },
-      weight: 1.3
+      weight: 1.3,
     },
     KeyQ: {
       eng: { central: 'q', secondary: 'Q' },
       rus: { central: 'й', secondary: 'Й' },
-      weight: 1
+      weight: 1,
     },
     KeyW: {
       eng: { central: 'w', secondary: 'W' },
       rus: { central: 'ц', secondary: 'Ц' },
-      weight: 1
+      weight: 1,
     },
     KeyE: {
       eng: { central: 'e', secondary: 'E' },
       rus: { central: 'у', secondary: 'У' },
-      weight: 1
+      weight: 1,
     },
     KeyR: {
       eng: { central: 'r', secondary: 'R' },
       rus: { central: 'к', secondary: 'К' },
-      weight: 1
+      weight: 1,
     },
     KeyT: {
       eng: { central: 't', secondary: 'T' },
       rus: { central: 'е', secondary: 'Е' },
-      weight: 1
+      weight: 1,
     },
     KeyY: {
       eng: { central: 'y', secondary: 'Y' },
       rus: { central: 'н', secondary: 'Н' },
-      weight: 1
+      weight: 1,
     },
     KeyU: {
       eng: { central: 'u', secondary: 'U' },
       rus: { central: 'г', secondary: 'Г' },
-      weight: 1
+      weight: 1,
     },
     KeyI: {
       eng: { central: 'i', secondary: 'I' },
       rus: { central: 'ш', secondary: 'Ш' },
-      weight: 1
+      weight: 1,
     },
     KeyO: {
       eng: { central: 'o', secondary: 'O' },
       rus: { central: 'щ', secondary: 'Щ' },
-      weight: 1
+      weight: 1,
     },
     KeyP: {
       eng: { central: 'p', secondary: 'P' },
       rus: { central: 'з', secondary: 'З' },
-      weight: 1
+      weight: 1,
     },
     BracketLeft: {
       eng: { central: '[', secondary: '{' },
       rus: { central: 'х', secondary: 'Х' },
-      weight: 1
+      weight: 1,
     },
     BracketRight: {
       eng: { central: ']', secondary: '}' },
       rus: { central: 'ъ', secondary: 'Ъ' },
-      weight: 1
+      weight: 1,
     },
     Backslash: {
       eng: { central: '\\', secondary: '|' },
       rus: { central: '\\', secondary: '/' },
-      weight: 1
+      weight: 1,
     },
     Delete: {
       eng: { central: 'del', secondary: 'del' },
       rus: { central: 'del', secondary: 'del' },
-      weight: 1
+      weight: 1,
     },
     CapsLock: {
       eng: { central: 'caps lock', secondary: 'caps lock' },
       rus: { central: 'caps lock', secondary: 'caps lock' },
-      weight: 2.3
+      weight: 2.3,
     },
     KeyA: {
       eng: { central: 'a', secondary: 'A' },
       rus: { central: 'ф', secondary: 'Ф' },
-      weight: 1
+      weight: 1,
     },
     KeyS: {
       eng: { central: 's', secondary: 'S' },
       rus: { central: 'ы', secondary: 'Ы' },
-      weight: 1
+      weight: 1,
     },
     KeyD: {
       eng: { central: 'd', secondary: 'D' },
       rus: { central: 'в', secondary: 'В' },
-      weight: 1
+      weight: 1,
     },
     KeyF: {
       eng: { central: 'f', secondary: 'F' },
       rus: { central: 'а', secondary: 'А' },
-      weight: 1
+      weight: 1,
     },
     KeyG: {
       eng: { central: 'g', secondary: 'G' },
       rus: { central: 'п', secondary: 'П' },
-      weight: 1
+      weight: 1,
     },
     KeyH: {
       eng: { central: 'h', secondary: 'H' },
       rus: { central: 'р', secondary: 'Р' },
-      weight: 1
+      weight: 1,
     },
     KeyJ: {
       eng: { central: 'j', secondary: 'J' },
       rus: { central: 'о', secondary: 'О' },
-      weight: 1
+      weight: 1,
     },
     KeyK: {
       eng: { central: 'k', secondary: 'K' },
       rus: { central: 'л', secondary: 'Л' },
-      weight: 1
+      weight: 1,
     },
     KeyL: {
       eng: { central: 'l', secondary: 'L' },
       rus: { central: 'д', secondary: 'Д' },
-      weight: 1
+      weight: 1,
     },
     Semicolon: {
       eng: { central: ';', secondary: ':' },
       rus: { central: 'ж', secondary: 'Ж' },
-      weight: 1
+      weight: 1,
     },
     Quote: {
       eng: { central: "'", secondary: '"' },
       rus: { central: 'э', secondary: 'Э' },
-      weight: 1
+      weight: 1,
     },
     Enter: {
       eng: { central: 'enter', secondary: 'enter' },
       rus: { central: 'enter', secondary: 'enter' },
-      weight: 2.3
+      weight: 2.3,
     },
     ShiftLeft: {
       eng: { central: 'shift', secondary: 'shift' },
       rus: { central: 'shift', secondary: 'shift' },
-      weight: 2.3
+      weight: 2.3,
     },
     KeyZ: {
       eng: { central: 'z', secondary: 'Z' },
       rus: { central: 'я', secondary: 'Я' },
-      weight: 1
+      weight: 1,
     },
     KeyX: {
       eng: { central: 'x', secondary: 'X' },
       rus: { central: 'ч', secondary: 'Ч' },
-      weight: 1
+      weight: 1,
     },
     KeyC: {
       eng: { central: 'c', secondary: 'C' },
       rus: { central: 'с', secondary: 'С' },
-      weight: 1
+      weight: 1,
     },
     KeyV: {
       eng: { central: 'v', secondary: 'V' },
       rus: { central: 'м', secondary: 'М' },
-      weight: 1
+      weight: 1,
     },
     KeyB: {
       eng: { central: 'b', secondary: 'B' },
       rus: { central: 'и', secondary: 'И' },
-      weight: 1
+      weight: 1,
     },
     KeyN: {
       eng: { central: 'n', secondary: 'N' },
       rus: { central: 'т', secondary: 'Т' },
-      weight: 1
+      weight: 1,
     },
     KeyM: {
       eng: { central: 'm', secondary: 'M' },
       rus: { central: 'ь', secondary: 'Ь' },
-      weight: 1
+      weight: 1,
     },
     Comma: {
       eng: { central: ',', secondary: '<' },
       rus: { central: 'б', secondary: 'Б' },
-      weight: 1
+      weight: 1,
     },
     Period: {
       eng: { central: '.', secondary: '>' },
       rus: { central: 'ю', secondary: 'Ю' },
-      weight: 1
+      weight: 1,
     },
     Slash: {
       eng: { central: '/', secondary: '?' },
       rus: { central: ',', secondary: '.' },
-      weight: 1
+      weight: 1,
     },
     ArrowUp: {
       eng: { central: '\u2191', secondary: '\u2191' },
       rus: { central: '\u2191', secondary: '\u2191' },
-      weight: 1
+      weight: 1,
     },
     ShiftRight: {
       eng: { central: 'shift', secondary: 'shift' },
       rus: { central: 'shift', secondary: 'shift' },
-      weight: 2.3
+      weight: 2.3,
     },
     ControlLeft: {
       eng: { central: 'ctrl', secondary: 'ctrl' },
       rus: { central: 'ctrl', secondary: 'ctrl' },
-      weight: 1.5
+      weight: 1.5,
     },
     MetaLeft: {
       eng: { central: 'win', secondary: 'win' },
       rus: { central: 'win', secondary: 'win' },
-      weight: 1
+      weight: 1,
     },
     AltLeft: {
       eng: { central: 'alt', secondary: 'alt' },
       rus: { central: 'alt', secondary: 'alt' },
-      weight: 1
+      weight: 1,
     },
     Space: {
       eng: { central: ' ', secondary: ' ' },
       rus: { central: ' ', secondary: ' ' },
-      weight: 6.9
+      weight: 6.9,
     },
     AltRight: {
       eng: { central: 'alt', secondary: 'alt' },
       rus: { central: 'alt', secondary: 'alt' },
-      weight: 1
+      weight: 1,
     },
     ControlRight: {
       eng: { central: 'ctrl', secondary: 'ctrl' },
       rus: { central: 'ctrl', secondary: 'ctrl' },
-      weight: 1.5
+      weight: 1.5,
     },
     ArrowLeft: {
       eng: { central: '\u2190', secondary: '\u2190' },
       rus: { central: '\u2190', secondary: '\u2190' },
-      weight: 1
+      weight: 1,
     },
     ArrowDown: {
       eng: { central: '\u2193', secondary: '\u2193' },
       rus: { central: '\u2193', secondary: '\u2193' },
-      weight: 1
+      weight: 1,
     },
     ArrowRight: {
       eng: { central: '\u2192', secondary: '\u2192' },
       rus: { central: '\u2192', secondary: '\u2192' },
-      weight: 1
-    }
+      weight: 1,
+    },
   };
 }
 
@@ -545,10 +536,9 @@ function isItShifted(toggle) {
   if (toggle) {
     localStorage.setItem('shift', !shift);
     return !shift;
-  } else {
-    localStorage.setItem('shift', shift);
-    return shift;
   }
+  localStorage.setItem('shift', shift);
+  return shift;
 }
 
 function getLang(toggle) {
@@ -567,10 +557,9 @@ function getLang(toggle) {
   if (toggle) {
     localStorage.setItem('state', !state);
     return !state;
-  } else {
-    localStorage.setItem('state', state);
-    return state;
   }
+  localStorage.setItem('state', state);
+  return state;
 }
 
 function isItalAlternated(toggle) {
@@ -589,10 +578,9 @@ function isItalAlternated(toggle) {
   if (toggle) {
     localStorage.setItem('alt', !alt);
     return !alt;
-  } else {
-    localStorage.setItem('alt', alt);
-    return alt;
   }
+  localStorage.setItem('alt', alt);
+  return alt;
 }
 
 function changeLang() {
@@ -601,12 +589,11 @@ function changeLang() {
   }
 }
 
-main.onkeyup = dimmer;
 function dimmer(e) {
   setTimeout(() => {
-    let key = document.getElementById(e.code);
+    const key = document.getElementById(e.code);
     key.classList.toggle('highlight');
-    if ((e.code === 'ShiftLeft') | (e.code === 'ShiftRight')) {
+    if ((e.code === 'ShiftLeft') || (e.code === 'ShiftRight')) {
       renderKeyboard(false, true);
     }
     if (e.code === 'AltLeft') {
@@ -615,11 +602,12 @@ function dimmer(e) {
   }, 0);
 }
 
-main.onkeydown = handle;
+main.onkeyup = dimmer;
+
 function handle(e) {
   const keys = keysGen();
   if (keys[e.code] !== undefined) {
-    let key = document.getElementById(e.code);
+    const key = document.getElementById(e.code);
     key.classList.add('highlight');
     if (e.code === 'CapsLock') {
       renderKeyboard(false, true);
@@ -627,11 +615,13 @@ function handle(e) {
     if (e.code === 'AltLeft') {
       isItalAlternated(true);
     }
-    if ((e.code === 'ShiftLeft') | (e.code === 'AltLeft')) {
+    if ((e.code === 'ShiftLeft') || (e.code === 'AltLeft')) {
       changeLang();
     }
-    if ((e.code === 'ShiftLeft') | (e.code === 'ShiftRight')) {
+    if ((e.code === 'ShiftLeft') || (e.code === 'ShiftRight')) {
       renderKeyboard(false, true);
     }
   }
 }
+
+main.onkeydown = handle;
